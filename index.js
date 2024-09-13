931071fe9d4a6253b5557bd4b2127dc09c234c64
masotnikov <masotnikov11@yandex.ru>
2024-09-13 13:37:10 +0300

    test postman

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ import express from 'express'
 const PORT = 5000;
 
 const app = express();
-app.get('/', (req, res) => {
-  console.log(req.query)
+app.use(express.json());
+app.post('/', (req, res) => {
+  console.log(req.body);
   res.status(200).json('Сервер работает')
 })
 app.listen(PORT, () => console.log('server working'));
